7149a09402f80c159abc684b4ae85a489df7b878
Irineu Rosa Junior <billzte@gmail.com>
2022-08-28 21:24:56 -0300

    envio teste de urls

--- procedimentos/views.py
+++ procedimentos/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import redirect, render, get_object_or_404
 from django.http import HttpResponse
 
 def procedimentos(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'home.html')
